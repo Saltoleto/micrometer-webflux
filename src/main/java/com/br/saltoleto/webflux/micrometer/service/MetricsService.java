c5426ed621ad7f2f758657aaa8a41de72661d753
Saltoleto <fernandosaltoleto@hotmail.com>
2023-09-06 15:16:33 -0300

    Create MetricsService.java

---
+++ src/main/java/com/br/saltoleto/webflux/micrometer/service/MetricsService.java
@@ -0,0 +1,32 @@
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class MetricsService {
+
+    private final MeterRegistry meterRegistry;
+
+    @Autowired
+    public MetricsService(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    public void recordTimerMetric(String metricName, long durationInMillis, Map<String, String> tags) {
+        List<Tag> micrometerTags = tags.entrySet().stream()
+                .map(entry -> Tag.of(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+
+        Timer timer = Timer.builder(metricName)
+                .description("Tempo gasto em alguma operação")
+                .tags(micrometerTags)
+                .register(meterRegistry);
+
+        timer.record(durationInMillis);
+    }
+}
